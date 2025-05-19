008b85daed71f149e59428fa5e263a022be339e9
ArchitAdh. <architadhikary@gmail.com>
2025-05-19 13:27:08 +0545

    rendered list usiing map funciton

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -1,9 +1,25 @@
 const AdminPanel = () => {
+  // rendering lists example
+  const products = [
+    { title: "phones", id: 1 },
+    { title: "laptop", id: 2 },
+    { title: "tablets", id: 3 },
+  ];
+
+  const listItems = products.map((product) => (
+    <li style={{ listStyle: "none" }} key={product.id}>
+      {product.title}
+    </li>
+  ));
+
   return (
     <div>
       <h1>AdminPanel</h1>
 
       <p>This is a basic admin panel</p>
+
+      <h2>Rendering lists using map</h2>
+      <ul>{listItems}</ul>
     </div>
   );
 };
